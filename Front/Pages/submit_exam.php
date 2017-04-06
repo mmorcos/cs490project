3e87ed1b7d867323783f34c66d1f6c17e1273473
Mina Morcos <mina@nwk-129-117.nwk.ruw.rutgers.edu>
2017-04-06 18:36:22 -0400

    Java answers compiling and retrieving output

--- Front/Pages/submit_exam.php
+++ Front/Pages/submit_exam.php
@@ -1,34 +1,4 @@
 <?php 
-/*
-$i=0; 
-for($i=0; $i<$counter; $i++)
-{
-	//$value[$i] = $_POST['value'];
-}
-
-$floc = 'people.txt';
-$dfile = fopen($floc, 'r');
-$tarr = fread($dfile, filesize($floc));
-$tarr = json_decode($tarr, 1);
-fclose($dfile);
-
-
-$file = 'people.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .= "John Smith\n";
-// Write the contents back to the file
-file_put_contents($file, $current);
-
-
-$myfile[] = fopen($file[$i], "w");      //This creates an array of files with index i and the "w" permmission 
-	fwrite($myfile, $value);		//this SHOULD write $value to $myfile
-	fclose($myfile);			
-
-*/
-
-
 
 $counter = sizeof($_POST['value']);
 $value = $_POST['value'];
@@ -38,10 +8,15 @@ $i=0;
 while($i<=sizeof($value)-1)
 {	
 
-	$file[$i] = 'answer' . $i . '.jar';       //This generates the file name
+	$file[$i] = 'answer' . $i . '.java';       //This generates the file name
 	echo $file;								 //This  just echos the filename to be created
-
-	file_put_contents($file[$i], $value[$i]); //This saves the files with their corresponding answers
+	file_put_contents($file[$i], "public class answer", FILE_APPEND);
+	file_put_contents($file[$i], $i, FILE_APPEND);
+	file_put_contents($file[$i], "{", FILE_APPEND);
+	file_put_contents($file[$i], $value[$i], FILE_APPEND); //This saves the files with their corresponding answers
+	file_put_contents($file[$i], "}", FILE_APPEND);
+	exec("javac $file");
+	$result[$i] = shell_exec("java answer");
 
 	$i++; 
 
